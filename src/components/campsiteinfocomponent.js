6e695a8905e4c4b335b16cb246fb0d1a1f415cde
Esteban Porras <estebanporras@Estebans-MBP.fios-router.home>
2021-08-09 14:41:59 -0400

    redux modal

--- src/components/campsiteinfocomponent.js
+++ src/components/campsiteinfocomponent.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody,
     Form, FormGroup, Input, Label } from 'reactstrap';
-import { Control, LocalForm } from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(+val);
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 
   function RenderCampsite({campsite}) {
       return (
@@ -128,7 +135,23 @@ class CommentForm extends Component {
                       name="author"
                       placeholder="First Name"
                       className="form-control"
+                      validators={{
+                        required,
+                        minLength: minLength(2),
+                        maxLength: maxLength(15)
+                      }}
                       />
+                      <Errors
+                              className="text-danger"
+                              model=".author"
+                              show="touched"
+                              component="div"
+                              messages={{
+                                required: "Required",
+                                minLength: "Must be at least 2 characters",
+                                maxLength: "Must be 15 characters or less",
+                              }}
+                            />
                   </div>
                   <div className="form-group">
                     <Label htmlFor="text">Comment</Label>
