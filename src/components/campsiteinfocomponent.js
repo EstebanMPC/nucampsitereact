66591058b060a5be2a979d21f70696c66c4120b0
Esteban Porras <estebanporras@Estebans-MBP.fios-router.home>
2021-08-12 18:25:51 -0400

    redux thunk

--- src/components/campsiteinfocomponent.js
+++ src/components/campsiteinfocomponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
     Form, FormGroup, Input, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './loadingcomponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -24,7 +25,7 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val
       );
     }
 
-  function RenderComments({comments}) {
+  function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -45,7 +46,7 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val
               </div>
             );
           })}
-          <CommentForm />
+          <CommentForm campsiteId={campsiteId} addComment={addComment} />
         </div>
       );
     }
@@ -53,7 +54,27 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val
   }
 
   function CampsiteInfo(props) {
-        if (props.campsite) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }    
+    if (props.campsite) {
         return (
             <div className= 'container'>
             <div className="row">
@@ -68,7 +89,11 @@ const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val
             </div>
                 <div className='row'>
                     <RenderCampsite campsite= {props.campsite} />
-                    <RenderComments comments= {props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
@@ -97,9 +122,8 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(this.state));
-        this.toggleModal()
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
   render(){
